4453fc88737540899b4b76b4cd5d13bcc8662190
Chris Nighitngale <ducatista@gmail.com>
2013-10-05 10:26:56 -0600

    interface improvments

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -3,6 +3,6 @@ class SeasonsController < ApplicationController
 	respond_to :json
 	
 	def index
-		respond_with(Season.order('year, subseason_id').all.as_json(only: ['id', 'current'], methods: :name))
+		respond_with(Season.order('year, subseason_id').all.as_json(only: ['id', 'current', 'subseason_id'], methods: :name))
 	end
 end
